3d930e9ed62eb36480a15ed87745bec288a4710f
harungokcegoz <harungokcegoz92@gmail.com>
2023-08-02 18:52:41 +0200

    Repo is Initialised

---
+++ CryptoApp/CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Harun Gökçegöz on 02/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
